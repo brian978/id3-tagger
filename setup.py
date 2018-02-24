ea04f58980210bbbbbae3e61d774552ee4c54bd2
brian978 <andrei.dincescu1@gmail.com>
2018-02-24 20:07:34 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='id3-tagger',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        'mutagen',
+    ],
     url='https://github.com/brian978/id3-tagger',
     license='BSD 3-Clause License',
     author='brian978',
